8173f99c1745d789e3f3769de7b08963a0e2627d
megarbry <megarbry@gmail.com>
2020-02-14 17:54:53 -0700

    added constructor to GameOutput

--- src/components/game/gameOutput.js
+++ src/components/game/gameOutput.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 
 export default class GameOutput extends Component {
+    constructor(props){
+        super(props);
+    }
 
     render () {
+        
     return (
         <div className='output-wrapper'>
 
